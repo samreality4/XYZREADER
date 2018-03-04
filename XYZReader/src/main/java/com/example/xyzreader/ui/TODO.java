4f8b7ea3d8b99617f8fc8d9df5c579f26f6df141
samreality4 <samreality4@gmail.com>
2018-03-04 09:42:08 -0800

    commit from windows

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/TODO.java
@@ -0,0 +1,12 @@
+package com.example.xyzreader.ui;
+
+/**
+ * Created by Sam on 3/4/2018.
+ */
+
+public class TODO {
+    //todo 01 fix the bar when i slide the viewpager
+    //todo 02 fix the lag
+    //todo 03 fix the bar size
+    //todo 04 do some with the FAB
+}
