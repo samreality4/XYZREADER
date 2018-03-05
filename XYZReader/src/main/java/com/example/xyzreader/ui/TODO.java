5d301ee286b87366147f9e9787855269af304839
sam <swannsee20@gmail.com>
2018-03-04 16:38:21 -0800

    commit from mac

--- XYZReader/src/main/java/com/example/xyzreader/ui/TODO.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/TODO.java
@@ -6,8 +6,10 @@ package com.example.xyzreader.ui;
 
 public class TODO {
     //todo 01 fix the bar when i slide the viewpager
-    //todo 02 fix the lag, some stuff is going on with the dB, fixed* by replace url
-    // "https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json"
+    //todo 02 fix the lag, some stuff is going on with the dB,
+    //  fixed* by replace url "https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json"
     //todo 03 fix the bar size
     //todo 04 do some with the FAB
+    //todo 05 fix the unknown space between collasping bar and title bar
+    //remove android:fitsSystemWindows="true"
 }
